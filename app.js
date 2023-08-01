ca4cf8a10eedca9e53a7701a8e3784563c624416
Milan Horínek <horinek@milanfon.cz>
2023-08-01 21:05:19 +0200

    Secondary adjustments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const XMLWriter = require('xml-writer');
 
+const input = require('./template.json');
+
 const stream = fs.createWriteStream('./output.svg');
 const writer = new XMLWriter(true, (str, enc) => stream.write(str, enc));
 
@@ -15,7 +17,7 @@ writer.writeAttribute('text-anchor', 'middle');
 writer.writeAttribute('dominant-baseline', 'hanging');
 writer.writeAttribute('font-family', 'Roboto Medium');
 writer.writeAttribute('font-size', '26pt');
-writer.text('CoD: MW II – 1080p – Balanced preset [FPS]');
+writer.text(input.name);
 writer.endElement();
 writer.endElement();
 writer.endDocument();
