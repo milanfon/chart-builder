70e7e5196b40ce91dd70c92ae01a008eaf838248
Milan Horínek <horinek@milanfon.cz>
2023-08-06 21:45:37 +0200

    Series names render

--- includes/page.js
+++ includes/page.js
@@ -9,13 +9,37 @@ class Page {
     id="text513">${this.props.name}</text>`;
     }
 
+    getVerticalScales() {
+        const n = this.props.series.length;
+        const start = 100/(n + 1);
+        return Array.apply(undefined, Array(n)).map((i , index) => (start*(index+1)).toFixed(1));
+    }
+
+    renderSeriesNames() {
+        const scales = this.getVerticalScales();
+        const n = this.props.series.length;
+        return [...Array(n).keys()].reduce((prev, curr) => prev + `
+            <text x="0" y="${scales[curr]}%" font-size="25pt" text-anchor="start" dominant-baseline="middle">
+            ${this.props.series[curr].name}
+            </text>
+        `, "");
+    }
+
+    renderLines() {
+
+    }
+
     render() {
+        console.log(this.getVerticalScales());
         return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg width="2000" height="1000" version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:svg="http://www.w3.org/2000/svg">
     ${this.renderName()} 
-</svg>`
+    <g transform="scale(0.9) translate(100 100)">
+        ${this.renderSeriesNames()}
+    </g>
+</svg>`;
     }
 }
 
