281e6795d18ef0173bf59aacb6a824e51c422fc1
Milan Horínek <horinek@milanfon.cz>
2024-08-24 10:00:00 +0200

    Completed parsing function

--- includes/parsers/displaycal.js
+++ includes/parsers/displaycal.js
@@ -2,10 +2,18 @@ export async function parseUniformityTest(filePath, inputName) {
     const file = Bun.file("input/"+inputName+"/"+filePath);
     const text = await file.text();
 
-    const match = text.match(/.*results = \{.*$/gm);
-    const resultsLine = match[0];
+    const resMatch = text.match(/.*results = \{.*$/gm);
+    const resultsLine = resMatch[0];
     const jsonStringMatch = resultsLine.match(/(\{.*\})/);
     const importedObject = eval("("+jsonStringMatch[0]+")");
 
-    Object.values(importedObject).forEach(v => console.log(v[0].XYZ[1]))
+    const values = Object.values(importedObject).map(v => Math.round(v[0].XYZ[1]));
+
+    const colsMatch = text.match(/cols\s*=\s*(\d+)\s*,/gm);
+    const cols = Number.parseInt(colsMatch[0].match(/\d+/)[0]);
+
+    const rowsMatch = text.match(/var\s+rows\s*=\s*\d+\s*,/gm);
+    const rows = Number.parseInt(rowsMatch[0].match(/\d+/)[0]);
+
+    return { rows, cols, values };
 }
