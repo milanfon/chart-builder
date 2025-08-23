906bbb8c1c7c3173fa769b5eb35a334f21214826
Milan Horínek <horinek@milanfon.cz>
2025-08-23 23:00:04 +0200

    Added option to change text size in table cells

--- includes/page.js
+++ includes/page.js
@@ -179,16 +179,25 @@ export class Page {
             params += `<line x1="${dimensions.specs.padding}" y1="${yPos}" x2="${dimensions.canvas.width - dimensions.specs.padding}" y2="${yPos}" stroke="#${colors.general.outline}" stroke-width="2"/>`;
             for (let c = 0; c < columnCount + 1; c++) {
                 if (i < 1 && c < 1) {
-                    params += `<text x="${dimensions.specs["param-line"] / 2 + dimensions.specs.padding}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.background}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"]}" dominant-baseline="central">Parametr</text>`
+                    params += `<text x="${dimensions.specs["param-line"] / 2 + dimensions.specs.padding}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.background}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"].default}" dominant-baseline="central">Parametr</text>`
                 }  else if (c < 1) {
-                    params += `<text x="${dimensions.specs.padding + dimensions.specs["param-line"] / 2}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.outline}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"]}" dominant-baseline="central">${this.props.parameters[i - 1]}</text>`;
+                    params += `<text x="${dimensions.specs.padding + dimensions.specs["param-line"] / 2}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.outline}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"].default}" dominant-baseline="central">${this.props.parameters[i - 1]}</text>`;
                 } else if (i > 0) {
-                    params += `<text x="${paramLine + (c - 0.5) * valUnit}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.outline}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"]}" dominant-baseline="central">${this.props.values[c - 1].val[i - 1]}</text>`;
+                    const cellValue = this.props.values[c - 1].val[i - 1];
+                    let [cellText, cellTextSize] = ["", dimensions.specs["font-size"].default];
+                    if (typeof cellValue === "object") {
+                        cellText = cellValue.text;
+                        cellTextSize = dimensions.specs["font-size"][cellValue.size];
+                    } else if(typeof cellValue === "string")
+                        cellText = cellValue;
+                    else
+                        console.log("Undefined type of cell");
+                    params += `<text x="${paramLine + (c - 0.5) * valUnit}" y="${headLine + (i+0.5) * unitHeight}" fill="#${colors.general.outline}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${cellTextSize}" dominant-baseline="central">${cellText}</text>`;
                 }
             }
         }
         for (let c = 1; c < columnCount + 1; c++) {
-            params += `<text x="${paramLine + (c - 0.5) * valUnit}" y="${headLine + (0.5) * unitHeight}" fill="#${colors.general.background}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"]}" dominant-baseline="central">${this.props.values[c - 1].name}</text>
+            params += `<text x="${paramLine + (c - 0.5) * valUnit}" y="${headLine + (0.5) * unitHeight}" fill="#${colors.general.background}" text-anchor="middle" align-baseline="middle" font-family="Russo One" font-size="${dimensions.specs["font-size"].default}" dominant-baseline="central">${this.props.values[c - 1].name}</text>
                         <line x1="${paramLine + c * valUnit}" y1="${dimensions.specs.padding}" x2="${paramLine + c * valUnit}" y2="${dimensions.canvas.height - dimensions.specs.padding}" stroke="#${colors.general.outline}" stroke-width="2"/>`;
             if (this.props.values[c-1].pic) {
                 params += `<image xlink:href="../../input/${this.inputName}/${this.props.values[c-1].pic.path}" x="${paramLine + (c - 0.9) * valUnit}" y="${dimensions.specs.padding + dimensions.specs["head-line"] * 0.1}" width="${valUnit * 0.8}" height="${dimensions.specs["head-line"] * 0.8}"/>`;
